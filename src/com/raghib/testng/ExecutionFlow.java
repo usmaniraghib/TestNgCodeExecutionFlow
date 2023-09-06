6f3134b0fce2a7ed48d7094fbcf507971052f14c
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:45:56 +0530

    Modified ExecutionFlow file

--- src/com/raghib/testng/ExecutionFlow.java
+++ src/com/raghib/testng/ExecutionFlow.java
@@ -3,6 +3,7 @@ package com.raghib.testng;
 /*
  * https://www.youtube.com/watch?v=qK2FBo2IAYQ
  */
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -14,7 +15,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /*
-
 beforeSuite
 beforeTest
 beforeClass
@@ -30,8 +30,8 @@ afterMethod
 afterClass
 afterTest
 afterSuite
+*/
 
- */
 public class ExecutionFlow {
 	
 	@Test
